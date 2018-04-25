b5f897d07a77c9e1500c451a3ae617d4c87fed07
carpediemliu <448148022@qq.com>
2018-04-25 23:53:53 +0800

    init

---
+++ src/main/top/vitoliu/singleton/MySingleton.java
@@ -0,0 +1,31 @@
+package main.top.vitoliu.singleton;
+
+/**
+ * 单例模式之双重锁检查方式
+ * <p>
+ * 由于new操作分为以下三步：
+ * 1.给instance分配内存
+ * 2.调用构造函数初始化变量
+ * 3.将instance指向分配的内存空间（执行完后instance就不为null了）
+ * <p>
+ * 由于JVM的即时编译器存在指令重排序的情况，上述顺序可能为1-2-3也可能为1-3-2。
+ * 所以，需要将instance设置为volatile，禁止指令的重排序
+ */
+public class MySingleton {
+
+    private volatile static MySingleton instance;
+
+    private MySingleton() {
+    }
+
+    public static MySingleton getSingleton() {
+        if (instance == null) {
+            synchronized (MySingleton.class){
+                if(instance == null){
+                    instance = new MySingleton();
+                }
+            }
+        }
+            return instance;
+    }
+}
---
+++ src/main/top/vitoliu/singleton/MyStaticSingleton.java
@@ -0,0 +1,20 @@
+package main.top.vitoliu.singleton;
+
+/**
+ * 采用静态内部类的方式（《Effective Java》推荐的方式）
+ * 由于SingletonHolder为线程私有的，除了通过getInstance()方法以外无法访问，所以它是懒汉式的，
+ * 由于实例的建立是在类加载的时候完成，故天生对多线程友好，无需使用同步
+ */
+public class MyStaticSingleton {
+    private MyStaticSingleton() {
+        System.out.println("MyStaticSingleton is create");
+    }
+
+    private static class SingletonHolder {
+        private static MyStaticSingleton instance = new MyStaticSingleton();
+    }
+
+    public static MyStaticSingleton getInstance() {
+        return SingletonHolder.instance;
+    }
+}
